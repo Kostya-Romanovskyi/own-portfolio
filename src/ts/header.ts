d36aa0a3f8907f94b6ca862b06fc47d5bfd5f1df
Konstantin Romanovskiy <konstantinromanovskiy@Konstantins-MacBook-Pro.local>
2025-09-17 00:05:55 -0700

    added styles for all screen sizes for header

--- src/ts/header.ts
+++ src/ts/header.ts
@@ -1,7 +1,14 @@
-const burgerButton = document.querySelector('.header__burger__btn');
+const burgerButton = document.querySelector(
+  '.header__burger__btn'
+) as HTMLButtonElement;
 
 burgerButton?.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   document.documentElement.classList.toggle('isOpen');
+
+  const ButtonClass = document.documentElement.classList.value;
+
+  if (ButtonClass === 'isOpen') {
+  }
 }
