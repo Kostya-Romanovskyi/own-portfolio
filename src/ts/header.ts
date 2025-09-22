2405e6514b7f7cce3e69d87c37b65853d7a13b29
Konstantin Romanovskiy <konstantinromanovskiy@Konstantins-MacBook-Pro.local>
2025-09-22 02:02:24 -0700

    added section projects, added section certificates

--- src/ts/header.ts
+++ src/ts/header.ts
@@ -2,8 +2,16 @@ const burgerButton = document.querySelector(
   '.header__burger__btn'
 ) as HTMLButtonElement;
 
+const navItems = document.querySelectorAll<HTMLLIElement>('.header__nav__item');
+
 burgerButton?.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   document.documentElement.classList.toggle('isOpen');
 }
+
+navItems.forEach(item =>
+  item.addEventListener('click', () => {
+    document.documentElement.classList.remove('isOpen');
+  })
+);
