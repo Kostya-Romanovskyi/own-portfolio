f9665f1fb99f78a5cef07c789935aac29234b78f
Konstantin Romanovskiy <konstantinromanovskiy@Konstantins-MacBook-Pro.local>
2025-10-05 14:39:20 -0700

    fixed script for animation sections

--- src/ts/gsap.ts
+++ src/ts/gsap.ts
@@ -5,6 +5,11 @@ gsap.registerPlugin(ScrollTrigger);
 
 const sections = document.querySelectorAll<HTMLElement>('.section');
 
+console.log(sections);
+sections.forEach(section => {
+  console.log(section);
+});
+
 sections.forEach(section => {
   gsap.from(section, {
     opacity: 0,
@@ -22,47 +27,53 @@ sections.forEach(section => {
 const aboutDecorateIcons =
   document.querySelectorAll<HTMLElement>('.decorate__icon');
 
-gsap.from(aboutDecorateIcons, {
-  opacity: 0,
-  x: 50,
-  duration: 1,
-  ease: 'power2.out',
-  stagger: 0.2,
-  scrollTrigger: {
-    trigger: aboutDecorateIcons[0],
-    start: 'top 80%',
-    toggleActions: 'play none none none',
-  },
+aboutDecorateIcons.forEach(icon => {
+  gsap.from(icon, {
+    opacity: 0,
+    x: 50,
+    duration: 1,
+    ease: 'power2.out',
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: icon,
+      start: 'top 80%',
+      toggleActions: 'play none none none',
+    },
+  });
 });
 
 const skillsItemSoft = document.querySelectorAll<HTMLElement>(
   '.skills__item--soft'
 );
 
-gsap.from(skillsItemSoft, {
-  opacity: 0,
-  x: 0,
-  duration: 1,
-  ease: 'power2.out',
-  stagger: 0.2,
-  scrollTrigger: {
-    trigger: skillsItemSoft[0],
-    start: 'top 80%',
-    toggleActions: 'play none none none',
-  },
+skillsItemSoft.forEach(item => {
+  gsap.from(item, {
+    opacity: 0,
+    x: 0,
+    duration: 1,
+    ease: 'power2.out',
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: item,
+      start: 'top 80%',
+      toggleActions: 'play none none none',
+    },
+  });
 });
 
 const skillsItem = document.querySelectorAll<HTMLElement>('.skills__item');
 
-gsap.from(skillsItem, {
-  opacity: 0,
-  x: 0,
-  duration: 1,
-  ease: 'power2.out',
-  stagger: 0.2,
-  scrollTrigger: {
-    trigger: skillsItem[0],
-    start: 'top 80%',
-    toggleActions: 'play none none none',
-  },
+skillsItem.forEach(item => {
+  gsap.from(item, {
+    opacity: 0,
+    x: 0,
+    duration: 1,
+    ease: 'power2.out',
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: item,
+      start: 'top 80%',
+      toggleActions: 'play none none none',
+    },
+  });
 });
